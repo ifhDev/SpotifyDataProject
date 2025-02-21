6c83553fae1be6aa2900001109328d8172aa36a0
IH <147421162+ifhDev@users.noreply.github.com>
2025-02-21 11:35:41 +0100

    created roadmap more EDA (danceability)

--- import clean dataset.py
+++ import clean dataset.py
@@ -3,8 +3,8 @@ import zipfile
 import kaggle
 
 # Set dataset name from Kaggle
-dataset = "maharshipandya/-spotify-tracks-dataset"
-data_dir = "data"
+dataset = 'maharshipandya/-spotify-tracks-dataset'
+data_dir = 'data'
 
 # Ensure the data directory exists
 os.makedirs(data_dir, exist_ok=True)
@@ -12,4 +12,4 @@ os.makedirs(data_dir, exist_ok=True)
 # Download dataset using Kaggle API
 kaggle.api.dataset_download_files(dataset, path=data_dir, unzip=True)
 
-print("Dataset downloaded and extracted to:", data_dir)
+print('Dataset downloaded and extracted to:', data_dir)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from spotifydataproject!")
+    print('Hello from spotifydataproject!')
 
 
 if __name__ == "__main__":
